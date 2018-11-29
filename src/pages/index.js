df9baa749a33c719ddc4f526f3e7c5b0b26b93d7
p0lax <polax@list.ru>
2018-11-29 22:04:58 +0100

    Gatsby canvas demo

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import Layout from '../components/Layout/Layout';
+import Drawer from '../components/Drawer/Drawer';
+
+const IndexPage = () => (
+  <Layout>
+    <Drawer />
+  </Layout>
+)
+
+export default IndexPage
